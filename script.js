d9ff42fa98c4e1f1e825bccd866b7f9b6b6dbb2a
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-03-13 12:53:49 -0400

    Implement game loop

--- script.js
+++ script.js
@@ -41,17 +41,23 @@ const gameBoard = (() => {
     return true;
   }
 
+  function checkHasEmptySpot() {
+    return grid.some((element) => {
+      return element === MARKS.empty;
+    });
+  }
+
   function clearBoard() {
     grid.fill(MARKS.empty);
   }
 
-  return { MARKS, grid, checkWin, markSpot, clearBoard };
+  return { MARKS, grid, checkWin, markSpot, clearBoard, checkHasEmptySpot };
 })();
 
 function createPlayer(mark) {
   let score = 0;
 
-  function checkScore() {
+  function getScore() {
     return score;
   }
 
@@ -63,24 +69,87 @@ function createPlayer(mark) {
     score = 0;
   }
 
-  return { mark, checkScore, incrementScore, resetScore };
+  return { mark, getScore, incrementScore, resetScore };
 }
 
 const game = (() => {
+  const GAME_STATES = {
+    ongoing: "ongoing",
+    win: "win",
+    tie: "tie",
+  };
   const player1 = createPlayer(gameBoard.MARKS.x);
   const player2 = createPlayer(gameBoard.MARKS.o);
 
   function playSpot(index, player) {
     gameBoard.markSpot(index, player.mark);
-    gameBoard.checkWin(player.mark);
+    return checkGameState(player);
   }
+
+  function incrementPlayerScore(player) {
+    player.incrementScore();
+  }
+
+  function endGame(gameState, player, force) {
+    if (force === true) {
+      gameBoard.clearBoard();
+      return true;
+    } else if (gameState === GAME_STATES.win || gameState === GAME_STATES.tie) {
+      gameBoard.clearBoard();
+      player.incrementScore();
+      return true;
+    }
+    return false;
+  }
+
+  function checkGameState(player) {
+    if (gameBoard.checkWin(player.mark) === true) {
+      endGame(GAME_STATES.win, player);
+      return GAME_STATES.win;
+    } else if (gameBoard.checkHasEmptySpot() === false) {
+      endGame(GAME_STATES.tie, player);
+      return GAME_STATES.tie;
+    } else return GAME_STATES.ongoing;
+  }
+
+  function resetGame() {
+    player1.resetScore();
+    player2.resetScore();
+    endGame(null, null, true);
+  }
+
+  return {
+    player1,
+    player2,
+    playSpot,
+    incrementPlayerScore,
+    checkGameState,
+    resetGame,
+  };
 })();
 
-gameBoard.markSpot(0, gameBoard.MARKS.o);
-console.log(gameBoard.checkWin(gameBoard.MARKS.o));
-gameBoard.markSpot(4, gameBoard.MARKS.o);
-gameBoard.markSpot(8, gameBoard.MARKS.o);
-console.log(gameBoard.checkWin(gameBoard.MARKS.o));
 console.log(gameBoard.grid);
-gameBoard.clearBoard();
+console.log(game.playSpot(0, game.player1));
+console.log(game.playSpot(1, game.player1));
+console.log(gameBoard.grid);
+console.log(game.playSpot(2, game.player1));
+console.log(gameBoard.grid);
+
+console.log(game.playSpot(0, game.player1));
+console.log(game.playSpot(1, game.player2));
+console.log(game.playSpot(2, game.player1));
+console.log(game.playSpot(3, game.player1));
+console.log(game.playSpot(4, game.player1));
+console.log(gameBoard.grid);
+console.log(game.player1.getScore());
+console.log(game.player2.getScore());
+game.resetGame();
+console.log(game.player1.getScore());
+console.log(game.player2.getScore());
+console.log(gameBoard.grid);
+console.log(game.playSpot(5, game.player2));
+console.log(game.playSpot(6, game.player2));
+console.log(game.playSpot(7, game.player1));
+console.log(gameBoard.grid);
+console.log(game.playSpot(8, game.player2));
 console.log(gameBoard.grid);
